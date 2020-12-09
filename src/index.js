ce1b49deace8b017809bcdd62911da3d0dc2b420
Djordje Vasic <djvasic@domain.local>
2020-12-09 16:03:41 +0100

    updating stripe interation and updating our favicon

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
+      <PersistGate persistor={persistor}>
         <App />
+      </PersistGate>  
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
