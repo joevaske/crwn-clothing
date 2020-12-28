cea0b883a49598a495ed414a2464bac6dd5d1cbb
Djordje Vasic <djvasic@domain.local>
2020-12-28 14:36:13 +0100

    implement code that pulls in our shop data from firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,15 +14,16 @@ const config = {
     measurementId: "G-QBM6B0C0K2"
   }
 
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
     const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = userRef.get();
+    const snapShot = await userRef.get();
 
-    if(!snapShot.exists) {
+   if(!snapShot.exists) {
         const{ displayName, email } = userAuth;
         const createdAt = new Date();
 
@@ -43,13 +44,38 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 }
 
+export const addCollectionsAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef);
 
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+    return await batch.commit();
+};
 
-  firebase.initializeApp(config);
+  export const convertCollectionsSnapshotToMap = collectionsSnapshot => {
+      const transformedCollection = collectionsSnapshot.docs.map(doc => {
+          const { title, items } = doc.data();
+
+          return {
+              routeName: encodeURI(title.toLowerCase()),
+              id: doc.id,
+              title,
+              items
+
+            };
+      });
+      return transformedCollection.reduce( (accumulator, collection) => {
+         accumulator[collection.title.toLowerCase()] = collection;
+         return accumulator;
+      }, {});
+  }
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
-
 
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({prompt: 'select_account'});
